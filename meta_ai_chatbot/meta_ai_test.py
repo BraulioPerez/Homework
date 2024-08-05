a5748a778fa8fb3ace8cb7d9085dad86e8d7806f
Braulio Perez <braulioperez.contacto@gmail.com>
2024-08-04 23:06:19 -0600

    clase funcional lista

--- meta_ai_chatbot/meta_ai_test.py
+++ meta_ai_chatbot/meta_ai_test.py
@@ -11,8 +11,32 @@ while True:
         break
 
 class psyc_assistant:
-    def __init__(self, user_input, last_prompt):
-        self.initial_prompt = """Act as an assistant for psicology department of the UPY, 
+    def __init__(self, user_input="N/A"):
+        self.initial_prompt = """
+                                Act as an assistant for psicology department of the UPY, 
                                 you need to gather my name and lastname from me, ask whatever 
                                 you want, if you understand start now, once you are done tell 
-                                me thats all for today, thank me and a smiley face :) onvr you are done, generate the following json: {Full name, nature of the problem, descriptio }"""
+                                me thats all for today, thank me and a smiley face :) once you are done
+                            """
+        self.user_input = user_input
+    
+    def get_response(self):
+        return ai.prompt(message=self.user_input)
+
+    def last_response(self):
+        return ai.prompt(message="generate the following json: {Full name, problem title, description }")
+    
+    def first_response(self):
+        return ai.prompt(message=self.initial_prompt)
+    
+if __name__ == "__main__":
+    assistant = psyc_assistant()
+    print(assistant.first_response())
+    while True:
+        assistant.user_input = input()
+        response = assistant.get_response()
+        print(response)
+        print(type(response))
+        if ":)" in response["message"]:
+            break
+        
