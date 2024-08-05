1bfc0ecc16273ce126f7c2705ef8000fddc071ab
Braulio Perez <braulioperez.contacto@gmail.com>
2024-08-05 15:40:10 -0600

    chat funcionando

--- meta_ai_chatbot/app.py
+++ meta_ai_chatbot/app.py
@@ -1,24 +1,19 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template
 from meta_ai_test import psyc_assistant
 
 app = Flask(__name__)
+assistant = psyc_assistant()
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('chat.html')
-
-@app.route('/start', methods=['GET'])
-def start():
-    assistant = psyc_assistant()
-    response = assistant.first_response()
-    return jsonify({'message': response})
-
-@app.route('/chat', methods=['POST'])
-def chat():
-    user_message = request.json.get('message')
-    assistant = psyc_assistant(user_input=user_message)
-    response = assistant.get_response()
-    return jsonify({'message': response})
+    if request.method == 'POST':
+        user_message = request.form['message']
+        assistant.user_input = user_message
+        response = assistant.get_response()
+        return render_template('chat.html', user_message=user_message, response=response["message"])
+    else:
+        initial_message = assistant.first_response()
+        return render_template('chat.html', initial_message=initial_message["message"])
 
 if __name__ == "__main__":
     app.run(debug=True)
