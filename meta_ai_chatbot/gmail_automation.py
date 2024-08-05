7834014d5320f27a55bb4b992c501ef33a3811da
Frank3040 <wxyz4150@gmail.com>
2024-08-04 19:09:25 -0600

    modifid gmail script

--- meta_ai_chatbot/gmail_automation.py
+++ meta_ai_chatbot/gmail_automation.py
@@ -9,7 +9,7 @@ def send_email_to(to_who, sender, data):
     "to": f"{to_who}",
     "sender": f"{sender}",
     "subject": "Student Concern",
-    "msg_html": f"<h1>Woah, my first email!</h1> <p>{data}</p>"
+    "msg_html": f"<h1>Student Data</h1> <p>{data}</p>"
     }
     message = gmail.send_message(**params)
 
